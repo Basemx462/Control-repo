8218960f83ca445b785791e473e41734fb6f67a7
basemx22 <31662252+basemx22@users.noreply.github.com>
2022-07-24 02:58:19 +0200

    Update ssh_server.pp

--- 04_07/end/control_repo/site/profile/manifests/ssh_server.pp
+++ 04_07/end/control_repo/site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDcolmaLu7YkExqcN6YzjyYmimLe3nslfQLGTUZbeZKcgcurxSfP08Hu1kENDKAeaYtUZFUPTmjbWda3SDTmxWF+V1n/RAR+zmbbs+gbNVTxRug8GMszS2Adrywdd2EjI70ikraxZ6sU2W6H8xxLhSrkyCLqFw2/tG6Sh9qeHJXneSW9QYktuN6MfPX78NaVYVkvq/deJio2k0Ce/FTHbXTMNzG10HCPzyFiEHDuLuK1VY1xi3OcmAqpDeq9ldn86q+OGtgT6GmzgOBfrboDUUMQlj6sdLZdt4/Xu+lAg9nOkqWxPjyb+bUdFcErCwNlRHLHd0ff50TgWr5GAhMk0cP',
   }  
 }
